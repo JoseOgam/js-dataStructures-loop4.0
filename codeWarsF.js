48ddf421ac94125cfc799b5196a7471a7a867350
JoseOgam <jotienoochieng@gmail.com>
2020-05-29 20:55:44 +0300

    :green_apple: codewars challange

---
+++ codeWarsF.js
@@ -0,0 +1,22 @@
+/*
+Create a function that returns the sum of the two lowest positive 
+numbers given an array of minimum 4 positive integers. No floats or 
+non-positive integers will be passed.
+*/
+function sumTwoSmallestNumbers(numbers) {
+    //Code here
+    let number = numbers.sort(function(b, a) { return b - a; }); //*** */
+    let result = 0;
+
+    for (let i = 0; i < number.length; i++) {
+        if (i === 0) {
+            result += number[0];
+        }
+        if (i === 1) {
+            result += number[1];
+        }
+    }
+    return result;
+};
+
+console.log(sumTwoSmallestNumbers([8, 3, 5, 1])); // 1 ,3 , 5, 8
