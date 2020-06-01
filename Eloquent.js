23104d783ca38c32be7e8eb83247bd10d60a563e
JoseOgam <jotienoochieng@gmail.com>
2020-06-01 07:54:42 +0300

    :green_apple: fixing my function

--- Eloquent.js
+++ Eloquent.js
@@ -1,51 +1,64 @@
-// //function definion
-// const square = function(x) { //one parameter
-//     return x * x;
-// }
-// console.log(square(3));
+//function definion
+const square = function(x) { //one parameter
+    return x * x;
+}
+console.log(square(3));
 
 
-// const makeNoise = function() { // no parameter
-//     console.log('ping');
-// }
+const makeNoise = function() { // no parameter
+    console.log('ping');
+}
 
-// makeNoise();
+makeNoise();
 
-// const power = function(base, exponent) { // more than one parameter
-//     let result = 1;
-//     for (let count = 0; count < exponent; count++) {
-//         result *= base;
-//     }
-//     return result;
-// }
+const power = function(base, exponent) { // more than one parameter
+    let result = 1;
+    for (let count = 0; count < exponent; count++) {
+        result *= base;
+    }
+    return result;
+}
 
-// console.log(power(2, 10));
+console.log(power(2, 10));
 
-// //Bindings and Scopes
-// let x = 10;
-// if (true) {
-//     let y = 20;
-//     var z = 30;
-//     result = x + y + z;
-//     console.log(result);
-// }
-// console.log(x + z);
+//Bindings and Scopes
+let x = 10;
+if (true) {
+    let y = 20;
+    var z = 30;
+    result = x + y + z;
+    console.log(result);
+}
+console.log(x + z);
 
 //Nested Scope
 
-const hummus = function(factor) {
-    const ingredient = function(amount, unit, name) {
-        let ingredientAmount = amount * factor;
-        if (ingredientAmount > 1) {
+const humms = function(factor) {
+    const ingredients = function(amount, unit, name) {
+        let ingredientsAmount = amount * factor;
+        if (ingredientsAmount > 1) {
             unit += 's';
         }
-        console.log(`${ingredientAmount} ${unit} ${name}`)
+        console.log(`${ingredientsAmount} ${unit} ${name}`)
     };
-    ingredient(1, "can", "chickpeas");
-    ingredient(0.25, "cup", "tahini");
-    ingredient(0.25, "cup", "lemon juice");
-    ingredient(1, "clove", "garlic");
-    ingredient(2, "tablespoon", "olive oil");
-    ingredient(0.5, "teaspoon", "cumin");
+    ingredients(0.5, 'cup', 'chicken');
+    ingredients(1, 'teaspoon', 'sugar');
 };
-console.log(hummus(2));
+
+console.log(humms(2));
+
+//Arrow function
+
+const squ = (x) => { return x * x };
+console.log(squ(5));
+
+const minus = (a, b) => {
+    if (b === undefined) {
+        return -a;
+    } else {
+        return a - b;
+    }
+}
+
+console.log(minus(10));
+console.log(minus(10, 5));
