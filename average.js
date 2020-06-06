0715850c8373360cc81399268393f07db9b35f12
JoseOgam <jotienoochieng@gmail.com>
2020-06-06 20:53:29 +0300

    :green_apple: more on datastructures

--- average.js
+++ average.js
@@ -1,6 +1,7 @@
+// Write function avg which calculates average of numbers in given list.
 // https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/javascript
-function find_average(array) {
-    var sum = array.reduce((a, b) => a + b, 0);
+function avg(array) {
+    let sum = array.reduce((a, b) => a + b);
     return sum / array.length;
 }
-console.log(find_average([2, 4, 1, 5]));
+console.log(avg([2, 4, 1, 5]));
---
+++ toString.js
@@ -0,0 +1,6 @@
+https: //www.codewars.com/kata/551f37452ff852b7bd000139/train/javascript
+    function addBinary(a, b) {
+        let result = a + b;
+        return result.toString(2);
+    }
+console.log(addBinary(3, 7));
