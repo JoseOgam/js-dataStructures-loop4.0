1503872e3e34b71c25cb8510dfe543a573d1def0
JoseOgam <jotienoochieng@gmail.com>
2020-06-28 09:39:11 -0400

    working on my datastructures

--- arr/array.js
+++ arr/array.js
@@ -26,10 +26,28 @@
 //         name: 'gabu'
 //     }, ];
 
-
 //     // let get = arr.find(item => item.id == 4);
 //     let fil = arr.filter(item => item.id < 5);
 //     return fil.length;
 //     // return get.name;
 // }
 // console.log(found());
+
+// let arr = [1, 2, 3, 4, 5]
+// arr.forEach(element => console.log(element))
+
+const loop = () => {
+  const items = ['item1', 'item2', 'item3']
+  const copyItems = ['item4']
+
+  let n = copyItems.concat(items)
+  return n
+  //   items.forEach(elements => console.log(elements))
+  //   for (let i = 0; i < items.length; i++) {
+  //     copyItems.push(items[i])
+  //   }
+  //   items.forEach(function (item) {
+  //     copyItems.push(items[item])
+  //   })
+}
+console.log(loop())
---
+++ odd.js
@@ -0,0 +1,6 @@
+let num = 1000;
+for (let i = 0; i < num; i++) {
+  if (i % 2 !== 0) {
+    console.log(`${i} is odd number`);
+  }
+}
