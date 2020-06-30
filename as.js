9e514ac6531ff2f67fe0b4d52ed03b1540c89871
JoseOgam <jotienoochieng@gmail.com>
2020-06-29 20:49:56 -0400

    working on my datastructures

---
+++ as.js
@@ -0,0 +1,5 @@
+function array(arr) {
+  //Good luck
+  //   arr.foreach((element) => (element[0] && element.length - 1).remove());
+}
+console.log(array("1,2,3"));
--- odd.js
+++ odd.js
@@ -1,6 +1,18 @@
-let num = 1000;
-for (let i = 0; i < num; i++) {
-  if (i % 2 !== 0) {
-    console.log(`${i} is odd number`);
-  }
+// let num = 1000;
+// for (let i = 0; i < num; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(`${i} is odd number`);
+//   }
+// }
+
+// function buildString(...template) {
+//   return `I like #{template.join(',')}`;
+// }
+
+function buildString(...template) {
+  return `I like ${template}`;
 }
+console.log(buildString("cheese", " milk", " honey"));
+
+// const myArray = [1, 2, NaN, 4];
+// console.log(myArray.indexOf(NaN) + myArray.indexOf(4));
