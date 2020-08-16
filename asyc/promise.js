1ff6e274a12dc10b6535d023e5d4dcbf937f7fed
JoseOgam <jotienoochieng@gmail.com>
2020-08-16 12:10:56 -0400

    object creation with constructor function

--- asyc/promise.js
+++ asyc/promise.js
@@ -1,16 +1,37 @@
-// try,catch and finally
+// // // try,catch and finally
 
-// try {
-//   throw new Error("my custom  errors");
-// } catch (err) {
-//   console.log("err", err);
-// } finally {
-//   console.log("this will alway executes");
+// // // try {
+// // //   throw new Error("my custom  errors");
+// // // } catch (err) {
+// // //   console.log("err", err);
+// // // } finally {
+// // //   console.log("this will alway executes");
+// // // }
+
+// // //creating a promise
+
+// // let promise = new Promise(function (resolve, reject) {
+// //   setTimeout(resolve, 100, "someValue");
+// // });
+// // console.log(promise);
+// // let journal = [];
+
+// // function addEntry(events, squirrel) {
+// //   return journal.push({ events, squirrel });
+// // }
+
+// // console.log(addEntry(["weekend", "cycling", "break", "peanuts", "beer"], true));
+
+// function Journal(events = [], squirrel) {
+//   this.events = events;
+//   this.squirrel = squirrel;
 // }
 
-//creating a promise
+// let addEntry = new Journal(
+//   ["weekend", "cycling", "break", "peanuts", "beer"],
+//   true
+// );
+// console.log(addEntry);
 
-let promise = new Promise(function (resolve, reject) {
-  setTimeout(resolve, 100, "someValue");
-});
-console.log(promise);
+let arr = ["weekend", "cycling", "break", "peanuts", "beer"];
+console.log(arr.length - 1);
