bc85063c3dfca9bad137d5f7ac2181617cffa61c
JoseOgam <jotienoochieng@gmail.com>
2020-11-11 14:12:53 -0500

    working on map and set datastructures

--- mapSet.js
+++ mapSet.js
@@ -26,12 +26,55 @@
 //  console.log(map.get("name"))
 
 
-const map = Object.create(null)
+// const map = Object.create(null)
 
-map.count = 1
+// map.count = 1
 
-if (map.count === 5) {
-    console.log("count key with numeric value")
-} else {
-    console.log("greater than 1")
-}
+// if (map.count === 5) {
+//     console.log("count key with numeric value")
+// } else {
+//     console.log("greater than 1")
+// }
+
+let set = new Set([1,2,3,4,5,6])
+// set.add(5)
+// set.add("5")
+
+// let key1 = {}
+// let key2 = {}
+
+// set.add(key1)
+// set.add(key2)
+
+// console.log(set)
+// console.log(set.has(key2))
+
+// set.forEach((key,value, ownerset) => {
+//     console.log(`${key} ${value}`)
+//     console.log(ownerset === set)
+    
+
+// })
+
+// let array = [...set]
+// console.log(array)
+
+let map = new Map()
+
+let key1 = {}
+let key2 = {}
+
+map.set(key1, 5)
+map.set(key2, 45)
+map.set('name', 'Joseph')
+map.set('age', 26)
+
+
+// console.log(map.get("name"))
+// console.log(map.has("name"))
+// console.log(map.size)
+
+map.forEach((items,value, ownermap) => {
+    console.log(`${items} ${value}`)
+    console.log(ownermap === map)
+})
