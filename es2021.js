b494c8b5ac48d3c79b99faebc8d1fadc873cd945
JoseOgam <jotienoochieng@gmail.com>
2021-07-01 17:51:32 -0400

    es2021

---
+++ es2021.js
@@ -0,0 +1,24 @@
+const person = { name: 'Joseph', position: 'Junior frontend developer', sex: "" }
+
+person.name ||= 'user'
+console.log(person.name)
+
+person.sex ||= 'Not specified'
+console.log(person.sex)
+
+// using if statement
+if (!person.name)
+{
+    person.name = 'user'
+}
+console.log(person.name)
+
+if (!person.sex)
+{
+    person.sex = 'sex not specified'
+}
+console.log(person.sex)
+
+//or
+
+person.
