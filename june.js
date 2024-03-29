2b091c30ac449dda0f7065f446783c0eeb98c02a
Joseph <jotienoochieng@gmail.com>
2024-03-29 09:37:50 +0300

    new feature

--- june.js
+++ june.js
@@ -1,29 +1,29 @@
-const mwezi = function(month) {
-    // let month = 6;
-    if (month === 1) {
-        return 'the month is January';
-    } else if (month === 2) {
-        return 'the month is February';
-    } else if (month === 3) {
-        return 'the month is March';
-    } else if (month === 4) {
-        return 'the month is April';
-    } else if (month === 5) {
-        return 'the month is May';
-    } else if (month === 6) {
-        return 'the month is June and June 5th is my Birthday';
-    } else if (month === 7) {
-        return 'the month is July';
-    } else if (month === 8) {
-        return 'the month is August';
-    } else if (month === 9) {
-        return 'the month is September';
-    } else if (month === 10) {
-        return 'the month is October';
-    } else if (month === 11) {
-        return 'the month is November';
-    } else if (month === 12) {
-        return 'the month is December';
-    }
-}
-console.log(mwezi(6));
+const mwezi = function (month) {
+  // let month = 6;
+  if (month === 1) {
+    return "the month is January";
+  } else if (month === 2) {
+    return "the month is February";
+  } else if (month === 3) {
+    return "the month is March";
+  } else if (month === 4) {
+    return "the month is April";
+  } else if (month === 5) {
+    return "the month is May";
+  } else if (month === 6) {
+    return "the month is June and June 5th is my Birthday";
+  } else if (month === 7) {
+    return "the month is July";
+  } else if (month === 8) {
+    return "the month is August";
+  } else if (month === 9) {
+    return "the month is September";
+  } else if (month === 10) {
+    return "the month is October";
+  } else if (month === 11) {
+    return "the month is November";
+  } else if (month === 12) {
+    return "the month is December";
+  }
+};
+console.log(mwezi(11));
